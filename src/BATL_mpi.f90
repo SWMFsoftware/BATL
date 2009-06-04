57cb90307d722187b5e50636ebc25477880b8c4e
gtoth <>
2009-06-04 19:36:23 +0000

    init_mpi: iComm is optional. Added clean_mpi

--- src/BATL_mpi.f90
+++ src/BATL_mpi.f90
@@ -6,21 +6,47 @@ module BATL_mpi
 
   SAVE
 
-  integer:: nProc, iProc, iComm
+  private ! except
+
+  public:: init_mpi       ! initialize this module
+  public:: clean_mpi      ! finalize MPI
+  public:: barrier_mpi    ! use an MPI barrier to synchronize processors
+
+  integer, public:: iComm ! MPI communicator for the group of processors
+  integer, public:: nProc ! number of processors in this group
+  integer, public:: iProc ! processor rank from 0 to nProc-1
 
 contains
 
   !==========================================================================
   subroutine init_mpi(iCommIn)
 
-    integer, intent(in):: iCommIn
+    ! Initialize iComm, nProc and iProc. If iCommIn is not present, set it
+    ! to MPI_COMM_WORLD and also call MPI_init
+
+    integer, optional, intent(in):: iCommIn
     integer :: iError
     !------------------------------------------------------------------------
-    iComm = iCommIn
+    if(.not.present(iCommIn))then
+       call MPI_init(iError)
+       iComm = MPI_COMM_WORLD
+    else
+       iComm = iCommIn
+    end if
     call MPI_COMM_RANK (iComm, iProc, iError)
     call MPI_COMM_SIZE (iComm, nProc, iError)
 
   end subroutine init_mpi
+  !==========================================================================
+  subroutine clean_mpi
+
+    ! This should only be called if the whole application is finished
+
+    integer :: iError
+    !------------------------------------------------------------------------
+    call MPI_finalize(iError)
+
+  end subroutine clean_mpi
   !==========================================================================
   subroutine barrier_mpi
 
