3ec8cab184b3cee0bb7e13ad01abd1fdfac16c64
gtoth <>
2009-06-07 07:42:47 +0000

    Moved some variables from BATL_tree into BATL_geometry. Added init_geometry.

--- src/BATL_geometry.f90
+++ src/BATL_geometry.f90
@@ -1,7 +1,48 @@
 module BATL_geometry
 
+  use BATL_size, ONLY: MaxDim, nDim
+
   implicit none
 
-  logical :: IsCartesian = .true.
+  SAVE
+
+  private ! except
+
+  public:: init_geometry
+
+  character(len=20), public:: TypeGeometry = 'cartesian'
+
+  ! Cartesian, cylindrical or spherical coordinates
+  logical, public:: IsCartesian   = .true.
+  logical, public:: IsSpherical   = .false.
+  logical, public:: IsCylindrical = .false.
+
+  ! Periodicity of the domain per dimension
+  logical, public:: IsPeriodic_D(MaxDim) = .false.
+
+contains
+
+  subroutine init_geometry(TypeGeometryIn, IsPeriodicIn_D)
+
+    character(len=*), optional, intent(in):: TypeGeometryIn
+    logical,          optional, intent(in):: IsPeriodicIn_D(nDim)
+    !-----------------------------------------------------------------------
+
+    TypeGeometry = 'cartesian'
+    if(present(TypeGeometryIn)) TypeGeometry = TypeGeometryIn
+
+    IsCartesian   = TypeGeometry(1:9)  == 'cartesian'
+    IsSpherical   = TypeGeometry(1:9)  == 'spherical'
+    IsCylindrical = TypeGeometry(1:11) == 'cylindrical'
+
+    ! Set default for periodicity
+    IsPeriodic_D = .false.
+    if(IsCylindrical) IsPeriodic_D = (/ .false., .true., .false./)
+    if(IsSpherical)   IsPeriodic_D = (/ .false., .false., .true./)
+
+    ! Set periodicity if argument is provided
+    if(present(IsPeriodicIn_D)) IsPeriodic_D(1:nDim) = IsPeriodicIn_D
+
+  end subroutine init_geometry
 
 end module BATL_geometry
