ec42efe46a8c205c8dbc65e17f4cb11bfcd1ec00
gtoth <>
2009-09-26 23:56:11 +0000

    iBlockSend -> iBlock, iNodeSend -> iNode, do_prolong -> do_receive Removed unused variables. Added more comments.

--- src/BATL_pass_face.f90
+++ src/BATL_pass_face.f90
@@ -62,8 +62,8 @@ contains
     integer :: iDim, iDimSide, iRecvSide, iSign
     integer :: iSend, jSend, kSend, iSide, jSide, kSide
     integer :: iDir, jDir, kDir
-    integer :: iNodeRecv, iNodeSend
-    integer :: iBlockRecv, iProcRecv, iBlockSend, iProcSend, DiLevel
+    integer :: iNodeRecv, iNode
+    integer :: iBlockRecv, iProcRecv, iBlock, iProcSend, DiLevel
 
     ! Fast lookup tables for index ranges per dimension
     integer, parameter:: Min_=1, Max_=2
@@ -71,7 +71,6 @@ contains
 
     ! Index range for recv and send segments of the blocks
     integer :: iRMin, iRMax, jRMin, jRMax, kRMin, kRMax
-    integer :: iSMin, iSMax, jSMin, jSMax, kSMin, kSMax
 
     ! Variables related to recv and send buffers
     integer, parameter:: nFaceCell = max(nI*nJ,nI*nK,nJ*nK)
@@ -85,7 +84,7 @@ contains
     integer, allocatable, save:: iRequestR_I(:), iRequestS_I(:), &
          iStatus_II(:,:)
 
-    logical:: DoTest, DoTestMe
+    logical:: DoTest
     character(len=*), parameter:: NameSub = 'BATL_pass_face::message_pass_face'
     !--------------------------------------------------------------------------
     DoTest = .false.; if(present(DoTestIn)) DoTest = DoTestIn
@@ -118,15 +117,15 @@ contains
     end if
 
     ! Loop through all blocks
-    do iBlockSend = 1, nBlock
+    do iBlock = 1, nBlock
 
-       if(Unused_B(iBlockSend)) CYCLE
+       if(Unused_B(iBlock)) CYCLE
 
-       iNodeSend = iNode_B(iBlockSend)
+       iNode = iNode_B(iBlock)
 
        if(present(MinLevelSendIn))then
           ! Do not send or receive below MinLevelSendIn-1
-          if(iTree_IA(Level_,iNodeSend) < MinLevelSendIn - 1) CYCLE
+          if(iTree_IA(Level_,iNode) < MinLevelSendIn - 1) CYCLE
        end if
 
        do iDim = 1, nDim
@@ -141,7 +140,7 @@ contains
              kDir = iSign*i_DD(iDim,3)
 
              ! Check for resolution change
-             DiLevel = DiLevelNei_IIIB(iDir,jDir,kDir,iBlockSend)
+             DiLevel = DiLevelNei_IIIB(iDir,jDir,kDir,iBlock)
 
              ! For res. change send flux from fine side to coarse side
              if(DoResChangeOnly .and. DiLevel == 0) CYCLE
@@ -151,15 +150,15 @@ contains
              elseif(DiLevel == 1)then
                 if(present(MinLevelSendIn))then
                    ! Do not send below MinLevelSendIn
-                   if(iTree_IA(Level_,iNodeSend) < MinLevelSendIn) CYCLE
+                   if(iTree_IA(Level_,iNode) < MinLevelSendIn) CYCLE
                 end if
                 call do_restrict
              elseif(DiLevel == -1)then
-                call do_prolong
+                call do_receive
              endif
           end do ! iDimSide
        end do ! iDim
-    end do ! iBlockSend
+    end do ! iBlock
 
     ! Done for serial run
     if(nProc == 1) RETURN
@@ -325,15 +324,15 @@ contains
       ! if the receiver block fills into the 
       ! lower or upper part of the face
 
-      iSide = 0; if(iRatio==2) iSide = modulo(iTree_IA(Coord1_,iNodeSend)-1, 2)
-      jSide = 0; if(jRatio==2) jSide = modulo(iTree_IA(Coord2_,iNodeSend)-1, 2)
-      kSide = 0; if(kRatio==2) kSide = modulo(iTree_IA(Coord3_,iNodeSend)-1, 2)
+      iSide = 0; if(iRatio==2) iSide = modulo(iTree_IA(Coord1_,iNode)-1, 2)
+      jSide = 0; if(jRatio==2) jSide = modulo(iTree_IA(Coord2_,iNode)-1, 2)
+      kSide = 0; if(kRatio==2) kSide = modulo(iTree_IA(Coord3_,iNode)-1, 2)
 
       iSend = (3*iDir + 3 + iSide)/2
       jSend = (3*jDir + 3 + jSide)/2
       kSend = (3*kDir + 3 + kSide)/2
 
-      iNodeRecv  = iNodeNei_IIIB(iSend,jSend,kSend,iBlockSend)
+      iNodeRecv  = iNodeNei_IIIB(iSend,jSend,kSend,iBlock)
       iProcRecv  = iTree_IA(Proc_,iNodeRecv)
       iBlockRecv = iTree_IA(Block_,iNodeRecv)
 
@@ -373,6 +372,7 @@ contains
 
       if(iProc == iProcRecv)then
 
+         ! Direct copy
          do iR2 = iR2Min, iR2Max
             iS2Min = 1 + Dn2*(iR2-iR2Min)
             iS2Max = iS2Min + Dn2 - 1
@@ -383,11 +383,13 @@ contains
                   Flux_VFB(iVar,iR1,iR2,iRecvSide,iBlockRecv) = &
                        Flux_VFB(iVar,iR1,iR2,iRecvSide,iBlockRecv) - &
                        sum(Flux_VFB(iVar,iS1Min:iS1Max,iS2Min:iS2Max,&
-                       iDimSide,iBlockSend))
+                       iDimSide,iBlock))
                end do
             end do
          end do
       else
+
+         ! Send via buffer
          iBufferS = iBufferS_P(iProcRecv)
 
          if(iBufferS + DnBuffer > MaxBufferS) call extend_buffer( &
@@ -409,7 +411,7 @@ contains
                do iVar = 1, nVar
                   BufferS_IP(iBufferS+iVar,iProcRecv) = &
                        sum(Flux_VFB(iVar,iS1Min:iS1Max,iS2Min:iS2Max,&
-                       iDimSide,iBlockSend))
+                       iDimSide,iBlock))
                end do
                iBufferS = iBufferS + nVar
             end do
@@ -419,13 +421,13 @@ contains
       end if
 
       ! Zero out the flux that was just sent/copied
-      Flux_VFB(:,:,:,iDimSide,iBlockSend) = 0.0
+      Flux_VFB(:,:,:,iDimSide,iBlock) = 0.0
 
     end subroutine do_flux
 
     !==========================================================================
 
-    subroutine do_prolong
+    subroutine do_receive
 
       integer :: nSize
       !------------------------------------------------------------------------
@@ -438,11 +440,13 @@ contains
             do iSide = (1-iDir)/2, 1-(1+iDir)/2
                iSend = (3*iDir + 3 + iSide)/2
 
-               iNodeRecv  = iNodeNei_IIIB(iSend,jSend,kSend,iBlockSend)
+               iNodeRecv  = iNodeNei_IIIB(iSend,jSend,kSend,iBlock)
                iProcRecv  = iTree_IA(Proc_,iNodeRecv)
-
+               
+               ! Same processor gets direct copy
                if(iProc == iProcRecv) CYCLE
 
+               ! Calculate size of the face
                select case(iDim)
                case(1)
                   nSize = nVar*nJ*nK/(jRatio*kRatio)
@@ -457,7 +461,7 @@ contains
          end do
       end do
 
-    end subroutine do_prolong
+    end subroutine do_receive
 
     !==========================================================================
 
@@ -627,7 +631,7 @@ contains
 
     use BATL_size, ONLY: nI, nJ, nK, MinI, MaxI, MinJ, MaxJ, MinK, MaxK, &
          MaxBlock
-    use BATL_tree, ONLY: DiLevelNei_IIIB, iTree_IA, Level_, iNode_B
+    use BATL_tree, ONLY: DiLevelNei_IIIB
     use BATL_geometry, ONLY: IsCartesian
     use BATL_grid, ONLY: CellVolume_B, CellVolume_GB
 
@@ -711,13 +715,12 @@ contains
   subroutine test_pass_face
 
     use BATL_mpi,  ONLY: iProc
-    use BATL_size, ONLY: MaxDim, nDim, iRatio, jRatio, kRatio, &
-         MinI, MaxI, MinJ, MaxJ, MinK, MaxK, nG, nI, nJ, nK, nBlock
+    use BATL_size, ONLY: MaxDim, nDim, nI, nJ, nK, nBlock
     use BATL_tree, ONLY: init_tree, set_tree_root, find_tree_node, &
          refine_tree_node, distribute_tree, show_tree, clean_tree, &
          Unused_B, DiLevelNei_IIIB
     use BATL_grid, ONLY: init_grid, create_grid, clean_grid, &
-         Xyz_DGB, CellSize_DB, CellFace_DB
+         Xyz_DGB, CellFace_DB
     use BATL_geometry, ONLY: init_geometry
 
     integer, parameter:: MaxBlockTest            = 50
