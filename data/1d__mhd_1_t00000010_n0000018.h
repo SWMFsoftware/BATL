910d1ebe536c4ccffe22c32d158c6e723039bb2b
gtoth <>
2012-09-10 18:03:02 +0000

    Added PARAM file and output for 1D test.

---
+++ data/1d__mhd_1_t00000010_n0000018.h
@@ -0,0 +1,19 @@
+GM/IO2/1d__mhd_1_t00000010_n0000018.h                                           
+       4 nProc
+      18 n_step
+  1.00000E+01 t
+ -6.4000000000E+01  6.4000000000E+01 -1.0000000000E-06  1.0000000000E-06 -1.0000000000E-06  1.0000000000E-06 plot_range
+ -1.0000000000E+00 -1.0000000000E+00 -1.0000000000E+00  1.6000000000E+00  1.0000000000E+00  1.0000000000E+00        48 plot_dx, dxmin, ncell
+      11 nplotvar
+       2 neqpar
+  1.66667E+00  3.00000E+00
+rho ux uy uz bx by bz p jx jy jz g rbody
+normalized variables
+       T save_binary
+       8 nByteReal
+cartesian           
+real4     
+      10       1       1 nRoot_D
+       4       1       1 nIJK_D
+       F       F       F IsPeriodic_D
+  2.99790E+08 -0.00000E+00  3.00000E+00 cLight, ThetaTilt, rBody
